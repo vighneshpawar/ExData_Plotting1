d5292bb4e5839063ac7b04d45149519ad04bd29d
vighneshpawar <vighnesh.mitmpl2022@learner.manipal.edu>
2025-02-17 20:56:25 +0530

    plot2 code

---
+++ plot2.R
@@ -0,0 +1,10 @@
+library("data.table")
+setwd("C:/Users/91892/Downloads/exdata_data_household_power_consumption")
+powerDT <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+png("plot2.png", width=480, height=480)
+plot(x = powerDT[, dateTime], y = powerDT[, Global_active_power]
+     , type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
